b501cebbee11d4d3a9f0170c77d1e22794c9ac7d
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2024-10-07 12:17:36 -0500

    completed and working
    changed Main.java to EmployeeDemo.java + added comments

--- week8/lab-1.1.8/Employee.java
+++ week8/lab-1.1.8/Employee.java
@@ -1,51 +1,80 @@
-
+// Name: Lucy Finnerty
+// Date: 10/7/24
+// Purpose: This class is for creating Employee objects. It contains three different constructors for use,
+// as well as accessor and mutator methods to retrieve Employee details or set them to something new.
 public class Employee {
     
-    String name;
-    int idNumber;
-    String department;
-    String position;
+    String name; // holds employee's name
+    int idNumber; // employee's id number
+    String department; // the department employee works in
+    String position; // employee's work position within department
 
-    // 3 constructors
+    // constructor with 4 parameters - name, id number, department, position
     public Employee(String name, int idNumber, String department, String position) {
         this.name = name;
         this.idNumber = idNumber;
         this.department = department;
         this.position = position;
     }
+    // contructor with 2 parameters -- name and id number
     public Employee(String name, int idNumber) {
         this.name = name;
         this.idNumber = idNumber;
-        this.department = "";
-        this.position = "";
+        this.department = ""; // set to empty string
+        this.position = ""; // set to empty string
     }
+    // constructor with no args -- string variables are empty and id number is set to 0
     public Employee() {
         this.name = "";
         this.idNumber = 0;
         this.department = "";
         this.position = "";
     }
+    /*
+     * method that accepts a string to set to 'name' variable
+     */
     public void setName(String name) {
         this.name = name;
     }
+    /*
+     * method that returns 'name' variable
+     */
     public String getName() {
         return name;
     }
+    /*
+     * method that accepts an int to set to 'idNumber' variable
+     */
     public void setIdNumber(int idNumber) {
         this.idNumber = idNumber;
     }
+    /*
+     * method that returns 'idNumber' variable
+     */
     public int getIdNumber() {
         return idNumber;
     }
+    /*
+     * method that accepts a string to set to 'department' variable
+     */
     public void setDepartment(String department) {
         this.department = department;
     }
+    /*
+     * method that returns 'department' variable
+     */
     public String getDepartment() {
         return department;
     }
+    /*
+     * method that accepts a string to set to 'position' variable
+     */
     public void setPosition(String position) {
         this.position = position;
     }
+    /*
+     * method that returns 'position' variable
+     */
     public String getPosition() {
         return position;
     }
--- week8/lab-1.1.8/EmployeeDemo.java
+++ week8/lab-1.1.8/EmployeeDemo.java
@@ -1,17 +1,24 @@
-
-public class Main {
+// Name: Lucy Finnerty
+// Date: 10/7/24
+// Purpose: This class demos all three constructors and displays them. The first one requires all 4 parameters, the second requires 2,
+// and the third does not require any.
+public class EmployeeDemo {
     public static void main(String[] args) {
-        // create 3 Employee objects
+        // 3 Employee objects using all three different constructors
+        // constructor 1
         Employee one = new Employee("Susan Meyers", 47899, "Accounting", "Vice President");
+        // constructor 2
         Employee two = new Employee("Mark Jones", 39119);
         two.setDepartment("IT");
         two.setPosition("Programmer");
+        //constructor 3
         Employee three = new Employee();
         three.setName("Joy Rogers");
         three.setIdNumber(81774);
         three.setDepartment("Manufacturing");
         three.setPosition("Engineer");
 
+        // displays all 3 employees
         System.out.println("    Name         ID       Department       Position");
         System.out.println("----------------------------------------------------------");
         System.out.println(one.getName() + "    " + one.getIdNumber() + "     " + one.getDepartment() + "       " + one.getPosition());
