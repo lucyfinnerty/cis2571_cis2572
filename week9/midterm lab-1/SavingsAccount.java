cea04e716fe8109275cd0bbf41c918ec3b49547f
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2024-10-21 13:38:19 -0500

    completed and working
    grammatical error

--- week9/midterm lab-1/SavingsAccount.java
+++ week9/midterm lab-1/SavingsAccount.java
@@ -1,6 +1,6 @@
 // Name: Lucy Finnerty
 // Date: 10/17/24
-// Purpose: This class allows for the creation of a saving account where a starting balance must be provided.
+// Purpose: This class allows for the creation of a savings account where a starting balance must be provided.
 // It has an accessor and mutator method, as well as a withdraw, deposit, and monthly interest capability.
 public class SavingsAccount {
      private double balance; // total amount of money in savings account
