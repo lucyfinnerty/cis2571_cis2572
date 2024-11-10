2602a4e3bc17a4ca4c10a9fa5f4dab1af7aec72a
lucyfinnerty <114194409+lucyfinnerty@users.noreply.github.com>
2024-11-10 13:40:12 -0600

    completed and working

--- week12/lab-1.1.12/TicTacToeBoard.java
+++ week12/lab-1.1.12/TicTacToeBoard.java
@@ -1,6 +1,6 @@
 // Name: Lucy Finnerty
 // Date: 11/5/24
-// Purpose:
+// Purpose: This program displays a Tic-Tac-Toe board where each cell is randomly decided to be either X, O, or empty.
 import java.util.Random;
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -10,26 +10,39 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 
 public class TicTacToeBoard extends Application {
+
     private static final int SIZE = 3; // tic-tac-toe board size
+    
     public static void main(String[] args) {
-        // creates stage and calls the start method
+        // calls the start method
         launch(args);
     }
+    /**
+     * Called and ran from the main method to start the program.
+     * Creates Tic-Tac-Toe grid and fills each cell with an X, O,
+     * or empty space based on a randomized value.
+     * 
+     * @param primaryStage the top level container (the window)
+     */
     @Override
     public void start(Stage primaryStage) {
+        // create new grid object for board
         GridPane grid = new GridPane();
+        // load 2 new image objects set to x and o gifs
         Image xImage = new Image("x.gif");
         Image oImage = new Image("o.gif");
-        Random random = new Random();
-
+        Random random = new Random(); // new random object
+        
+        // nested loops to fill 3x3 grid
         for(int row=0; row < SIZE; row++) {
             for(int col=0; col < SIZE; col++) {
+                // value set to 0, 1, or 2
                 int value = random.nextInt(3);
-                if(value == 0) {
+                if(value == 0) { // if value is 0 -> load X gif
                     grid.add(new ImageView(xImage), col, row);
-                } else if(value == 1) {
+                } else if(value == 1) { // if value is 1 -> load O gif
                     grid.add(new ImageView(oImage), col, row);
-                } // else cell is empty
+                } // else (value is 2) cell is empty
             }
         }
         Scene scene = new Scene(grid, 150, 150); // create new scene
--- week12/lab-1.2.12/Checkerboard.java
+++ week12/lab-1.2.12/Checkerboard.java
@@ -1,6 +1,6 @@
 // Name: Lucy Finnerty
 // Date: 11/5/24
-// Purpose:
+// Purpose: This program displays an 8x8 Checkerboard, which has 64 cells alternating the color black and white.
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
@@ -9,17 +9,30 @@ import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class Checkerboard extends Application {
-    private static final int CELL_SIZE = 50;
-    private static final int BOARD_SIZE = 8;
+    
+    private static final int CELL_SIZE = 50; // size of each cell
+    private static final int BOARD_SIZE = 8; // size of board
+    
     public static void main(String[] args) {
+        // calls the start method
         launch(args);
     }
+    /**
+     * Called and ran from the main method to start the program.
+     * Creates the board (grid) and has 64 rectangle objects within
+     * that alternates between the color black and white.
+     * 
+     * @param primaryStage the top level container (the window)
+     */
     @Override
     public void start(Stage primaryStage) {
+        // new grid object for the board
         GridPane grid = new GridPane();
 
+        // nested for loops to fill the 8x8 grid
         for(int row=0; row < BOARD_SIZE; row++) {
             for(int col=0; col < BOARD_SIZE; col++) {
+                // each cell is a rectangle object set to CELL_SIZE
                 Rectangle cell = new Rectangle(CELL_SIZE, CELL_SIZE);
                 // alternate colors based on the row and column
                 if ((row + col) % 2 == 0) {
